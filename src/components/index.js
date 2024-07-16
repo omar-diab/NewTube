d766bdf83d2626bd72708d978eb95d42f910c8fc
omar-diab <omar.i.s.diab@gmail.com>
2024-07-16 19:45:44 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import ChannelDetail from "./channelDetail/ChannelDetail";
+import Feed from "./feed/Feed";
+import Navbar from "./navbar/Navbar";
+import SearchFeed from "./searchFeed/SearchFeed";
+import VideoDetail from "./videoDetail/VideoDetail";
+import SearchBar from "./searchBar/SearchBar";
+import SideBar from './sidebar/SideBar';
+import Videos from "./videosfolder/Videos";
+import VideoCard from "./videoCard/VideoCard";
+import ChannelCard from "./channelCard/ChannelCard";
+import Loader from "./loader/Loader";
+
+export {
+    ChannelDetail,
+    Feed,
+    Navbar,
+    SearchFeed,
+    VideoDetail,
+    SearchBar,
+    SideBar,
+    Videos,
+    VideoCard,
+    ChannelCard,
+    Loader
+}
